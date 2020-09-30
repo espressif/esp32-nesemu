c81c4e1d2010f8cede5d97bb95ce3e383e875ca8
moononournation <moononournation@gmail.com>
2020-09-30 17:03:50 +0800

    align to arduino-esp32 default partition schema

--- main/main.c
+++ main/main.c
@@ -8,26 +8,26 @@
 #include "nofrendo.h"
 #include "esp_partition.h"
 
-
-
-char *osd_getromdata() {
-	char* romdata;
-	const esp_partition_t* part;
+char *osd_getromdata()
+{
+	char *romdata;
+	const esp_partition_t *part;
 	spi_flash_mmap_handle_t hrom;
 	esp_err_t err;
 	nvs_flash_init();
-	part=esp_partition_find_first(0x40, 1, NULL);
-	if (part==0) printf("Couldn't find rom part!\n");
-	err=esp_partition_mmap(part, 0, 3*1024*1024, SPI_FLASH_MMAP_DATA, (const void**)&romdata, &hrom);
-	if (err!=ESP_OK) printf("Couldn't map rom part!\n");
+	part = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_1, NULL);
+	if (part == 0)
+		printf("Couldn't find rom part!\n");
+	err = esp_partition_mmap(part, 0, 0x140000, SPI_FLASH_MMAP_DATA, (const void **)&romdata, &hrom);
+	if (err != ESP_OK)
+		printf("Couldn't map rom part!\n");
 	printf("Initialized. ROM@%p\n", romdata);
-    return (char*)romdata;
+	return (char *)romdata;
 }
-
 
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-    return ESP_OK;
+	return ESP_OK;
 }
 
 int app_main(void)
@@ -36,6 +36,5 @@ int app_main(void)
 	nofrendo_main(0, NULL);
 	printf("NoFrendo died? WtF?\n");
 	asm("break.n 1");
-    return 0;
+	return 0;
 }
-
