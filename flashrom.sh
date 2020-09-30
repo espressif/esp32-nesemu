2de36692f45569670a4496f67acb623d1460c2c8
moononournation <moononournation@gmail.com>
2020-09-30 22:24:19 +0800

    align to arduino-esp32 default partition schema

--- flashrom.sh
+++ flashrom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # . ${IDF_PATH}/add_path.sh
-esptool.py --chip esp32 --port "/dev/cu.SLAB_USBtoUART" --baud $((230400*4)) write_flash -fs 4MB 0x100000 "$1"
+esptool.py --chip esp32 --port "/dev/cu.SLAB_USBtoUART" --baud $((230400*4)) write_flash -fs 4MB 0x150000 "$1"
